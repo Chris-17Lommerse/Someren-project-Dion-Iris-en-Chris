fa45004f9c266284587f467b60f750d08d61de10
TheQuincey <i.castroelfvering@gmail.com>
2025-03-12 11:12:26 +0100

    activities

---
+++ Applicatie/Someren-Applicatie/Someren-Applicatie/Controllers/ActivitiesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Someren_Applicatie.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Activity> activities =
+                [
+                new Activity(1,"Test", new DateTime(2025, 3, 23)),
+                new Activity(2, "Test2", new DateTime(2025, 4, 1))
+                ,
+                ];
+
+            //pass the list to the View
+            return View(activities);
+        }
+    }
+}
