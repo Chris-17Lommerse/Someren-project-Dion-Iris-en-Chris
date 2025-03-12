b7fe39e57ba0bff27300b828ab91b2d1c2f4ff08
Dion Verhoef <51044594+dion123445@users.noreply.github.com>
2025-03-12 10:27:34 +0100

    student model

---
+++ Applicatie/Someren-Applicatie/Someren-Applicatie/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace Someren_Applicatie.Models
+{
+    public class Student
+    {
+        public int StudentNr { get; set; }
+        public string Voornaam { get; set; }
+        public string Achternaam { get; set; }
+        public string TelefoonNr { get; set; }
+        public string KamerNr { get; set; }
+
+        public Student()
+        {
+            StudentNr = 0;
+            Voornaam = "";
+            Achternaam = "";
+            TelefoonNr = "";
+            KamerNr = "";
+        }
+
+        public Student(int studentNr, string voornaam, string achternaam, string telefoonNr, string kamerNr)
+        {
+            StudentNr = studentNr;
+            Voornaam = voornaam;
+            Achternaam = achternaam;
+            TelefoonNr = telefoonNr;
+            KamerNr = kamerNr;
+        }
+    }
+}
