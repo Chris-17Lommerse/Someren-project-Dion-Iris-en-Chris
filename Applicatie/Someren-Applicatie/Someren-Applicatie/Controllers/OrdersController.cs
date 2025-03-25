377a682f1ed5df9aa91af74375e6393d6776e723
ChrisLommerse12 <89691283+Chris-17Lommerse@users.noreply.github.com>
2025-03-25 10:29:42 +0100

    Verder gegaan met het bestellen van een drankje

--- Applicatie/Someren-Applicatie/Someren-Applicatie/Controllers/OrdersController.cs
+++ Applicatie/Someren-Applicatie/Someren-Applicatie/Controllers/OrdersController.cs
@@ -1,152 +1,45 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Someren_Applicatie.Models;
+using Someren_Applicatie.Repositories.Orders;
 using Someren_Applicatie.Repositories.Rooms;
 
 namespace Someren_Applicatie.Controllers
-{ 
-//    public class OrdersController : Controller
-//    {
-//        // Repository variable will be declared
-//        private readonly IRoomRepository _roomsRepository;
+{
+    public class OrdersController : Controller
+    {
+        private readonly IOrdersRepository _ordersRepository;
 
-//        // Constructor
-//        public RoomsController(IRoomRepository roomsRepository)
-//        {
-//            _roomsRepository = roomsRepository;
-//        }
+        public OrdersController(IOrdersRepository ordersRepository)
+        {
+            _ordersRepository = ordersRepository;
+        }
 
-//        public IActionResult Index(string? searchString)
-//        {
-//            try
-//            {
-//                List<Room> rooms;
-//                // Get all rooms from the database and return the Index View of the Activities
-//                if (!string.IsNullOrEmpty(searchString) && (!searchString.Contains("Kies een optie")))
-//                {
-//                    rooms = _roomsRepository.GetByRoomSize(searchString);
-//                }
-//                else
-//                {
-//                    rooms = _roomsRepository.GetAll();
-//                }
-//                return View(rooms);
-//            }
-//            catch (Exception ex)
-//            {
-//                throw new Exception(ex.Message);
-//            }
-//        }
+        public IActionResult Index()
+        {
+            List<Order> orders = _ordersRepository.GetAll();
+            return View(orders);
+        }
 
-//        // GET: RoomsController/Create
-//        [HttpGet]
-//        public ActionResult Create()
-//        {
-//            // Returns a create form to add a room
-//            return View();
-//        }
+        [HttpGet]
+        public IActionResult AddOrder()
+        {
+            return View();
+        }
 
-//        // POST: RoomsController/Create
-//        [HttpPost]
-//        public ActionResult Create(Room room)
-//        {
-//            try
-//            {
-//                // add room via repository
-//                _roomsRepository.Add(room);
+        [HttpPost]
+        public IActionResult AddOrder(Order order)
+        {
+            try
+            {
+                _ordersRepository.Add(order);
 
-//                // Go back to list (Via Index)
-//                return RedirectToAction("Index");
-//            }
-//            catch (Exception ex)
-//            {
-//                // Something went wrong, go back to Index
-//                ViewBag.ErrorMessage = ex.Message;
-//                return View(room);
-//            }
-//        }
-
-//        // GET: RoomsController/Edit/5
-//        [HttpGet]
-//        public ActionResult Edit(string? id)
-//        {
-//            try
-//            {
-//                // 404 Not Found error
-//                if (id == null)
-//                {
-//                    return NotFound();
-//                }
-
-//                // get room via repository
-//                Room? room = _roomsRepository.GetById((string)id);
-//                return View(room);
-//            }
-//            catch (Exception ex)
-//            {
-//                throw new Exception(ex.Message);
-//            }
-//        }
-
-//        [HttpPost]
-//        // POST: RoomsController/Edit
-//        public ActionResult Edit(Room room)
-//        {
-//            try
-//            {
-//                // Edit room via repository
-//                _roomsRepository.Update(room);
-
-//                // Go back to list (Via Index)
-//                return RedirectToAction("Index");
-//            }
-//            catch (Exception ex)
-//            {
-//                // Something went wrong, go back to Index
-//                ViewBag.ErrorMessage = ex.Message;
-//                return View(room);
-//            }
-//        }
-//        [HttpGet]
-//        // GET: RoomsController/Delete
-//        public ActionResult Delete(string? id)
-//        {
-//            try
-//            {
-//                // 404 Not Found error
-//                if (id == null)
-//                {
-//                    return NotFound();
-//                }
-
-//                // get room via repository
-//                Room? room = _roomsRepository.GetById((string)id);
-//                return View(room);
-//            }
-//            catch (Exception ex)
-//            {
-//                {
-//                    throw new Exception(ex.Message);
-//                }
-//            }
-//        }
-//        [HttpPost]
-//        // POST: RoomsController/Delete
-//        public ActionResult Delete(Room room)
-//        {
-//            try
-//            {
-//                // Delete room via repository
-//                _roomsRepository.Delete(room);
-
-//                // Go back to list (Via Index)
-//                return RedirectToAction("Index");
-//            }
-//            catch (Exception ex)
-//            {
-//                // Something went wrong, go back to Index
-//                ViewBag.ErrorMessage = ex.Message;
-//                return View(room);
-//            }
-//        }
-//    }
+                return RedirectToAction("Index");
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ErrorMessage = ex.Message;
+                return View(order);
+            }
+        }
+    }
 }
--- Applicatie/Someren-Applicatie/Someren-Applicatie/Repositories/Orders/DbOrdersRepository.cs
+++ Applicatie/Someren-Applicatie/Someren-Applicatie/Repositories/Orders/DbOrdersRepository.cs
@@ -1,10 +1,11 @@
-﻿using Someren_Applicatie.Models;
+﻿using Microsoft.Data.SqlClient;
+using Someren_Applicatie.Models;
 
 namespace Someren_Applicatie.Repositories.Orders
 {
     public class DbOrdersRepository : BaseRepository, IOrdersRepository
     {
-
+        const string BaseSelectQuery = "SELECT studentennr, drankid, aantal FROM BESTELLING";
         public DbOrdersRepository(IConfiguration configuration) : base(configuration)
         {
             
@@ -12,14 +13,53 @@ namespace Someren_Applicatie.Repositories.Orders
 
         public void Add(Order order)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                // Query to add a room to the database
+                string query = $"INSERT INTO BESTELLING (studentennr, drankid, aantal)" +
+                               "VALUES (@studentennr, @drankid, @aantal); " +
+                               "SELECT SCOPE_IDENTITY();";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@studentennr", order.StudentNr);
+                command.Parameters.AddWithValue("@drankid", order.DrankId);
+                command.Parameters.AddWithValue("@aantal", order.Aantal);
+
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
         }
 
         public List<Order> GetAll()
         {
-            throw new NotImplementedException();
+            List<Order> orders = new List<Order>();
+
+            using(SqlConnection connection = new SqlConnection(_connectionString))
+            {
+                string query = $"{BaseSelectQuery}";
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Connection.Open();
+                SqlDataReader reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    Order order = ReadOrder(reader);
+                    orders.Add(order);
+                }
+                reader.Close();
+            }
+            return orders;
         }
 
+
+        private Order ReadOrder(SqlDataReader reader)
+        {
+            Student studentNr = (Student)reader["studentennr"];
+            Drink drankId = (Drink)reader["drankid"];
+            int aantal = (int)reader["aantal"];
+
+            return new Order(studentNr, drankId, aantal);
+        }
         public Order? GetById(int orderId)
         {
             throw new NotImplementedException();
